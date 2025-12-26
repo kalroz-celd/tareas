1878266553824bb8e45310bc482e4de011b80b2a
Carlos Lagos <carlos.lagos.diaz@gmail.com>
2025-12-25 22:54:10 -0300

    Update

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,8 @@
 import "./bootstrap";
 import Alpine from "alpinejs";
 
+import Sortable from "sortablejs";
+window.Sortable = Sortable;
+
 window.Alpine = Alpine;
 Alpine.start();
--- resources/views/livewire/layout/navigation.blade.php
+++ resources/views/livewire/layout/navigation.blade.php
@@ -136,6 +136,13 @@
                 Tareas
             </a>
 
+            <a href="{{ route('clients.index') }}"
+               class="flex items-center gap-3 rounded-xl px-3 py-2 text-sm font-semibold
+                      text-slate-700 hover:bg-slate-100 dark:text-slate-300 dark:hover:bg-slate-900">
+                <span class="h-2 w-2 rounded-full bg-slate-300 dark:bg-slate-700"></span>
+                Clientes
+            </a>
+
             <a href="#"
                class="flex items-center gap-3 rounded-xl px-3 py-2 text-sm font-semibold
                       text-slate-700 hover:bg-slate-100 dark:text-slate-300 dark:hover:bg-slate-900">
